e6c3417e25c2fd153fafb9c315023ce134995c63
estevaobk <estevaobk@gmail.com>
2019-08-30 13:16:33 -0300

    Initial upload.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nopostman
+
+A very simple script used internally to authenticate against RH SSO using username/password and testing the OAuth2 flows on Red Hat 3scale.
---
+++ nopostman.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Usage: <client id> <client secret> <APIcast>
+
+USERNAME="rh-sso-user"
+
+PASSWORD="rhs-so-password"
+
+REALM="rh-sso-realm"
+
+RHSSO_HOST="https://rh-sso-host"
+
+
+########
+# Main #
+########
+
+
+clear
+
+RHSSO="${RHSSO_HOST}/auth/realms/${REALM}/protocol/openid-connect/token"
+
+export TKN=$(curl -k -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "username=${USERNAME}" -d "password=${PASSWORD}" -d "grant_type=password" -d "client_id=${1}" -d "client_secret=${2}" ${RHSSO} 2> /dev/null)
+
+echo -e "\nRH SSO: ${RHSSO}\n\nToken: ${TKN}\n"
+
+TKN=$(echo "${TKN}" | sed 's/.*access_token":"//g' | sed 's/".*//g')
+
+curl -k -v -X GET -H "Accept: application/json" -H "Authorization: Bearer ${TKN}" ${3}
